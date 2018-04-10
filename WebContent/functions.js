d7d122703e54aae61ab675199ad69b4904377f91
Kim Sandberg <s@ndberg.dk>
2018-04-10 05:28:13 +0200

    Function has a name submitButton

--- WebContent/functions.js
+++ WebContent/functions.js
@@ -4,7 +4,7 @@
 
 $(document).ready(function(){
 	
-	$("#LoginForm").submit(function() {
+	$("#LoginForm").submit(function submitButton() {
 		var username = $('#usernameField').val();
 		var password = $('#passwordField').val();
 		alert("username: " + username + "\n" + "password: " + password);
