f7168e1d5bc6d9f84167314900f8c10b9825d2f7
Frederik Martin <frederik@mybid.dk>
2018-04-27 12:09:14 +0200

    +Added createUser()

--- src/datalayer/UserDAOList.java
+++ src/datalayer/UserDAOList.java
@@ -1,5 +1,6 @@
 package datalayer;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import dtos.UserDTO;
@@ -7,18 +8,27 @@ import interfaces.IUserDAO;
 
 public class UserDAOList implements IUserDAO {
 
+	static List<UserDTO> list = new ArrayList<UserDTO>();
+
+	public UserDAOList() {
+
+	}
+
+	static {
+		list.add(new UserDTO(1, "Peter", "PP", "12312323", "Kode", Arrays.asList("Hund", "Gøgler af guds nåde")));
+		list.add(new UserDTO(2, "Gurli", "GG", "12312323", "Kode", Arrays.asList("Admin", "Gøgler af guds nåde")));
+		list.add(new UserDTO(3, "Madsen", "MM", "12312323", "Kode",
+				Arrays.asList("Admin", "Hund", "Gøgler af guds nåde")));
+		list.add(new UserDTO(4, "Tobin", "TT", "12312323", "Kode", Arrays.asList("Admin")));
+		list.add(new UserDTO(5, "Er", "EE", "12312323", "Kode", Arrays.asList("Bonde")));
+		list.add(new UserDTO(6, "Hanne", "HH", "12312323", "Kode", Arrays.asList("Hund")));
+		list.add(new UserDTO(7, "Ren", "RR", "12312323", "Kode", Arrays.asList("Admin", "Konge")));
+	}
+
 	@Override
 	public List<UserDTO> GetUserList() {
 
-		return Arrays.asList(
-				new UserDTO(1, "Peter", "PP", "12312323", "Kode", Arrays.asList("Hund", "Gøgler af guds nåde")),
-				new UserDTO(2, "Gurli", "GG", "12312323", "Kode", Arrays.asList("Admin", "Gøgler af guds nåde")),
-				new UserDTO(3, "Madsen", "MM", "12312323", "Kode",
-						Arrays.asList("Admin", "Hund", "Gøgler af guds nåde")),
-				new UserDTO(4, "Tobin", "TT", "12312323", "Kode", Arrays.asList("Admin")),
-				new UserDTO(5, "Er", "EE", "12312323", "Kode", Arrays.asList("Bonde")),
-				new UserDTO(6, "Hanne", "HH", "12312323", "Kode", Arrays.asList("Hund")),
-				new UserDTO(7, "Ren", "RR", "12312323", "Kode", Arrays.asList("Admin", "Konge")));
+		return list;
 
 	}
 
@@ -26,8 +36,14 @@ public class UserDAOList implements IUserDAO {
 	public UserDTO GetUser(int id) {
 		return GetUserList().get(--id);
 	}
+
 	@Override
 	public void deleteUser(int id) {
-		
+
+	}
+
+	@Override
+	public void createUser() {
+		list.add(new UserDTO(1000, "PUTIN", "PP", "12312323", "Kode", Arrays.asList("Hund", "Kejser")));
 	}
 }
--- src/interfaces/IUserDAO.java
+++ src/interfaces/IUserDAO.java
@@ -9,4 +9,5 @@ public interface IUserDAO {
 	List<UserDTO> GetUserList();
 	UserDTO GetUser(int id);
 	void deleteUser(int id);
+	void createUser();
 }
