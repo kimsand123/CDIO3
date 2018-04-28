2e5c6602929f2b44ff7933927a1e0859f80b6296
Frederik Martin <frederik@mybid.dk>
2018-04-29 00:48:18 +0200

    SE

--- src/rest/User.java
+++ src/rest/User.java
@@ -11,8 +11,13 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import datalayer.UserDAOList;
 import dtos.UserDTO;
 import interfaces.IUserDAO;
@@ -21,6 +26,10 @@ import rest.dtos.UserRestDTO;
 @Path("user")
 public class User {
 
+	@Context
+	Request request;
+	
+	
 	IUserDAO dao = null;
 
 	public User() {
@@ -66,16 +75,25 @@ public class User {
 
 	// @Produces(MediaType.APPLICATION_JSON)
 	@PUT
-	@Path("/update/{uid}")
+	@Path("/update")
 	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-	public Response updateUser(@PathParam("uid") int uid, String data) {
+	public Response updateUser(String data) {
 		
-		UserDTO user = dao.updateUser(uid, data);
+		
+		
+		
+		//ObjectMapper objectMapper = new ObjectMapper();
+		
+		
+		System.out.println(data);
+		
+		
+		//UserDTO user = dao.updateUser(uid, data);
 
 		// TODO: Check for null and if error return correct http error code
 
-		return Response.ok(createDTO(user), MediaType.APPLICATION_JSON).build();
+		return Response.ok("Data:" + data, MediaType.APPLICATION_JSON).build();
 	}
 
 	// @Produces(MediaType.APPLICATION_JSON)
